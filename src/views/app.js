be8a6c6f75722855a2cb0b5ce121971a6076dc97
fgkolf <fontas.gkolf@gmail.com>
2019-04-08 08:21:34 +0300

    logging middleware

--- src/views/app.js
+++ src/views/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
 import counter from '../reducers/counter'
+import { logMiddleware } from '../middlewares/log';
 import MyCounter from './counter';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,7 +11,9 @@ const store = createStore(
   counter,
   {},
   composeEnhancers(
-    applyMiddleware()
+    applyMiddleware(
+      logMiddleware
+    )
   )
 );
 
